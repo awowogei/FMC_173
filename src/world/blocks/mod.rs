9d03c622cc67744645b5b741d06980578fb3dba6
awowogei <125923016+awowogei@users.noreply.github.com>
2025-03-06 19:57:14 +0100

    Add chest

--- src/world/blocks/mod.rs
+++ src/world/blocks/mod.rs
@@ -1,5 +1,6 @@
 use fmc::prelude::*;
 
+mod chest;
 mod crafting_table;
 mod furnace;
 mod torch;
@@ -10,9 +11,10 @@ pub(super) struct BlocksPlugin;
 impl Plugin for BlocksPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(crafting_table::CraftingTablePlugin)
+            .add_plugins(chest::ChestPlugin)
             .add_plugins(furnace::FurnacePlugin)
-            .add_plugins(wheat::WheatPlugin)
             .add_plugins(torch::TorchPlugin)
-            .add_plugins(water::WaterPlugin);
+            .add_plugins(water::WaterPlugin)
+            .add_plugins(wheat::WheatPlugin);
     }
 }
