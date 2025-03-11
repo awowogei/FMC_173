adb464b0e6a8f0e169d9707c9481565547554a1e
awowogei <125923016+awowogei@users.noreply.github.com>
2025-03-11 19:43:02 +0100

    Add zombie

--- src/mobs/mod.rs
+++ src/mobs/mod.rs
@@ -2,10 +2,12 @@ use fmc::prelude::*;
 
 mod duck;
 mod pathfinding;
+mod zombie;
 
 pub struct MobsPlugin;
 impl Plugin for MobsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(duck::DuckPlugin);
+        app.add_plugins(duck::DuckPlugin)
+            .add_plugins(zombie::ZombiePlugin);
     }
 }
