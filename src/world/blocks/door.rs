b61a1cce17c8c7a6ff109673ccc1c71becaf312a
awowogei <125923016+awowogei@users.noreply.github.com>
2026-07-15 01:14:04 +0200

    Fix door not being rotated around the same point on server and client

--- src/world/blocks/door.rs
+++ src/world/blocks/door.rs
@@ -1,8 +1,7 @@
 use fmc::{
-    bevy::math::{DQuat, DVec3},
-    blocks::{BlockData, BlockPosition, BlockRotation, Blocks},
+    bevy::math::DQuat,
+    blocks::{BlockData, Blocks},
     prelude::*,
-    world::WorldMap,
 };
 
 use crate::players::HandInteractions;
@@ -31,42 +30,17 @@ fn spawn_function(commands: &mut EntityCommands, _block_data: Option<&BlockData>
 }
 
 fn handle_block_hits(
-    world_map: Res<WorldMap>,
     mut block_hits: Query<
-        (
-            Entity,
-            &mut Door,
-            &mut HandInteractions,
-            &mut Transform,
-            &BlockPosition,
-        ),
+        (Entity, &mut Door, &mut HandInteractions, &mut Transform),
         Changed<HandInteractions>,
     >,
 ) {
-    for (_entity, mut door, mut interactions, mut transform, block_position) in
-        block_hits.iter_mut()
-    {
-        let Some(block_state) = world_map.get_block_state(*block_position) else {
-            continue;
-        };
-
+    for (_entity, mut door, mut interactions, mut transform) in block_hits.iter_mut() {
         for _interaction in interactions.read() {
-            let inset = 1.0 / 16.0;
-            let offset = match block_state.rotation().unwrap() {
-                BlockRotation::Front => DVec3::new(1.0, 0.0, 0.0) + DVec3::new(-inset, 0.0, inset),
-                BlockRotation::Right => DVec3::new(0.0, 0.0, 0.0) + DVec3::new(inset, 0.0, inset),
-                BlockRotation::Back => DVec3::new(0.0, 0.0, 1.0) + DVec3::new(inset, 0.0, -inset),
-                BlockRotation::Left => DVec3::new(1.0, 0.0, 1.0) + DVec3::new(-inset, 0.0, -inset),
-            };
-            // The divide by 16 part is because we want to rotate around the point that is one
-            // pixel deep into the door.
-            let corner = block_position.as_dvec3() + offset;
             if door.open {
-                transform
-                    .rotate_around(corner, DQuat::from_rotation_y(-std::f64::consts::FRAC_PI_2));
+                transform.rotate(DQuat::from_rotation_y(-std::f64::consts::FRAC_PI_2));
             } else {
-                transform
-                    .rotate_around(corner, DQuat::from_rotation_y(std::f64::consts::FRAC_PI_2));
+                transform.rotate(DQuat::from_rotation_y(std::f64::consts::FRAC_PI_2));
             }
             door.open = !door.open;
         }
